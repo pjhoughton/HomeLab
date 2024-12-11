bbdfb4f253eb3d21a09fb700c414cc247d97accb
Paul <64659195+pjhoughton@users.noreply.github.com>
2024-12-11 13:27:15 +0000

    .

---
+++ Scripts/update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Get the hostname of the system 
+HOSTNAME=$(hostname)
+
+# Update package list
+sudo apt-get update
+
+# Upgrade all installed packages
+sudo aptitude safe-upgrade -y
+
+# Clean up unnecessary packages
+sudo apt-get autoremove -y
+
+if [[ "$HOSTNAME" == "docker" ]]; then 
+echo "Performing task A for $HOSTNAME" # Add the commands for task A here sudo apt-get update
+
+# update docker
+
+cd docker-compose
+docker compose pull
+
+elif [[ "$HOSTNAME" == "xo" ]]; then 
+echo "Performing task B for $HOSTNAME" 
+
+# update orchestra
+
+cd XenOrchestraInstallerUpdater
+sudo bash xo-install.sh --update
+
+fi
+
+
+echo "Completed updates for $HOSTNAME" 
+
+
+
